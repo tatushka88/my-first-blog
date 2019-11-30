262f0d371c47d2435ce70bc8a094c54e3d1c092d
tatushka88 <ya.onishhenkot88@gmail.com>
2019-11-30 17:14:09 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
+from django.shortcuts import render, get_object_or_404
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
